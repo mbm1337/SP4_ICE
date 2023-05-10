a0aca63c28f95034883720129a93e7bf5992318c
AgnerII <noah.0410@hotmail.com>
2023-05-10 11:53:14 +0200

    Onsdag

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PApplet.main("Main");
+
+    }
+    @Override
+    public void settings() {
+        size(400, 400);
+    }
+    @Override
+    public void setup(){
+        clear();
+    }
+    public void draw(){
+        clear();
+
     }
 }
