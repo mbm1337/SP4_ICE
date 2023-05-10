54d005447b07d5e225808fffaa764dd1081d7066
mbm1337 <mikkelsen96@live.dk>
2023-05-10 11:52:04 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
     public static void main(String[] args) {
+        PApplet.main("Main");
         System.out.println("Hello world!");
+    }
+
+
+    @Override
+    public void settings(){
+        size(300,300);
     }
 }
