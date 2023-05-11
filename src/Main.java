542f8556ac0c46765e0195f3b381a75f053e122c
AgnerII <noah.0410@hotmail.com>
2023-05-11 11:59:35 +0200

    Første indslag

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
 import processing.core.PApplet;
 
 public class Main extends PApplet {
+    int width = 1000;
+    int height = 800;
+    public static PApplet p;
     public static void main(String[] args) {
         PApplet.main("Main");
 
     }
     @Override
     public void settings() {
-        size(400, 400);
+        size(width, height);
     }
     @Override
     public void setup(){
-        clear();
+        p = this;
     }
     public void draw(){
-        clear();
+        Player player = new Player();
+        Lane lane = new Lane(200);
+        Lane lane2 = new Lane(400);
+        Lane lane3 = new Lane(600);
+        lane.draw();
+        player.draw();
+
 
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,10 @@ public class Player {
     private int score;
     private boolean haveWeapon;
     private int position;
+    private int width = 1000;
+    private int height = 800;
+    private int xPosition;
+    private int yPosition;
 
     public Player(){
 
@@ -23,6 +27,18 @@ public class Player {
 
     }
     public void onImpact(){
+
+    }
+    public void draw(){
+        xPosition = 450;
+        yPosition = 750;
+        Main.p.fill(150);
+        Main.p.rect(xPosition,yPosition,100,40);
+        Main.p.fill(100,70,50);
+        Main.p.rect(xPosition+35,yPosition+10,30,20);
+
+    }
+    public void keyPressed(){
 
     }
 
